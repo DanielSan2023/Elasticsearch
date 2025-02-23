ca5e3d358af1a43d292215013dda6bee176c282d
DanielSan2023 <electronic.san@gmail.com>
2025-02-23 19:07:39 +0100

    added Vehicle - entity class

--- src/main/java/com/elasticsearch/entity/Vehicle.java
+++ src/main/java/com/elasticsearch/entity/Vehicle.java
@@ -1,22 +1,33 @@
 package com.elasticsearch.entity;
 
+import com.elasticsearch.helper.Indices;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Document(indexName = "vehicle")
+@Document(indexName = Indices.VEHICLE_INDEX)
+@Setting(settingPath = "static/es-settings.json")
 public class Vehicle {
+    @Id
+    @Field(type = FieldType.Keyword)
     private String id;
+
     private String number;
+
+    @Field(type = FieldType.Text)
     private String name;
+
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date created;
-
 }
